34c77156dbc63d5154e9b98ba324bc7db02f8c5b
iliDaedalus <ili@ili-pro.local>
2016-11-05 22:07:55 +0100

    Hosted Domain
    By submitting “hostedDomain” parameter (e.g.
    hostedDomain:“mydomainname.com”) the google login will ask the user to
    login using an account under that domain.
    The account selection will display from the existing (in your device),
    only google accounts under that domain, making the selection of the
    account easier.
    
    Therfore this doesn’t stop the user from signing in with another
    account neither validates the account as member of the domain.
    The user will be able to proceed to login with with any google account
    if they go through the “add new account”.
    It is just up to the developer to do additional check, as google
    doesn’t provide an easy way to do it yet.
    
    Note: I think that in android the webClientId is required in order the
    hostedDomain to work, but in iOS it is not necessary.

--- src/android/GooglePlus.java
+++ src/android/GooglePlus.java
@@ -39,6 +39,7 @@ public class GooglePlus extends CordovaPlugin implements GoogleApiClient.OnConne
     public static final String ARGUMENT_WEB_CLIENT_ID = "webClientId";
     public static final String ARGUMENT_SCOPES = "scopes";
     public static final String ARGUMENT_OFFLINE_KEY = "offline";
+    public static final String ARGUMENT_HOSTED_DOMAIN = "hostedDomain";
 
     public static final String TAG = "GooglePlugin";
     public static final int RC_GOOGLEPLUS = 77552; // Request Code to identify our plugin's activities
@@ -141,6 +142,14 @@ public class GooglePlus extends CordovaPlugin implements GoogleApiClient.OnConne
             if (clientOptions.optBoolean(ARGUMENT_OFFLINE_KEY, false)) {
                 gso.requestServerAuthCode(webClientId, false);
             }
+        }
+
+        // Try to get hosted domain
+        String hostedDomain = clientOptions.optString(ARGUMENT_HOSTED_DOMAIN, null);
+
+        // if hostedDomain included, we'll request a hosted domain account
+        if (hostedDomain != null && !hostedDomain.isEmpty()) {
+            gso.setHostedDomain(hostedDomain);
         }
 
         //Now that we have our options, let's build our Client
