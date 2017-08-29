0ea2f910fce9eea0cc8ed76e0b5e3e712a62a9e3
Habibillah Ali <habibillah@gmail.com>
2017-08-29 20:21:16 +0700

    ensure callback run with correct context and after sdk fully loaded. creadit to @mzealey

--- src/browser/GooglePlusProxy.js
+++ src/browser/GooglePlusProxy.js
@@ -108,16 +108,16 @@ if (window.location.protocol === "file:") {
             gapi.auth2.init({
                 client_id: CLIENT_ID // CLIENT_ID is populated by the cordova after_prepare hook
             }).then(function () {
+                __googleSdkReady = true;
+
+                for (var i = 0; i < __googleCallbacks.length; i++) {
+                    __googleCallbacks[i].call(GooglePlusProxy);
+                }
+
                 // Listen for sign-in state changes.
                 gapi.auth2.getAuthInstance().isSignedIn.listen(GooglePlusProxy.updateSigninStatus);
             });
         });
-
-        __googleSdkReady = true;
-
-        for (var i = 0; i < __googleCallbacks.length; i++) {
-            __googleCallbacks[i]();
-        }
     };
 
     (function(d, s, id){
