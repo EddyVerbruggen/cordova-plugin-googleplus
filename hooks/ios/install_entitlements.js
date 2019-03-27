9bdfcf87062cd1e6e8dd8bc2b31f73c11605344d
Gerrit van Huyssteen <gerrit.vanhuyssteen@britehouse.co.za>
2019-03-27 10:28:14 +0200

    chore: Support Cordova 9

--- hooks/ios/install_entitlements.js
+++ hooks/ios/install_entitlements.js
@@ -7,7 +7,7 @@ var xcode = require('xcode'),
     util = require('util');
 
 module.exports = function (context) {
-  var Q = context.requireCordovaModule('q');
+  var Q = require('q');
   var deferral = new Q.defer();
 
   if (context.opts.cordova.platforms.indexOf('ios') < 0) {
