78458d87ef40720d5e7698edc367229258588f1d
Adriano Di Giovanni <me@adrianodigiovanni.com>
2019-05-31 11:19:05 +0200

    fix #591

--- hooks/ios/prerequisites.js
+++ hooks/ios/prerequisites.js
@@ -1,8 +1,17 @@
 console.error("Google Sign-In prerequisites");
 
 module.exports = function (context) {
-  var child_process = require('child_process'),
-      deferral = require('q').defer();
+  var child_process = require('child_process');
+
+  var Q
+
+  try {
+    Q = require('q');
+  } catch (e) {
+    Q = context.requireCordovaModule('q');
+  }
+
+  var deferral = Q.defer();
 
   var output = child_process.exec('npm install', {cwd: __dirname},
       function (error) {
