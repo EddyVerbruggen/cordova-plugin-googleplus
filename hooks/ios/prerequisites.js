e7b99b7629a96f8b5ec920b81004cacdb9305241
Mike Nelson <mike.nelson@beweb.co.nz>
2019-08-29 02:40:21 +1200

    fixed cordova 9 error when using requireCordovaModule

--- hooks/ios/prerequisites.js
+++ hooks/ios/prerequisites.js
@@ -2,7 +2,7 @@ console.error("Google Sign-In prerequisites");
 
 module.exports = function (context) {
   var child_process = context.requireCordovaModule('child_process'),
-      deferral = context.requireCordovaModule('q').defer();
+      deferral = require('q').defer();
 
   var output = child_process.exec('npm install', {cwd: __dirname},
       function (error) {
