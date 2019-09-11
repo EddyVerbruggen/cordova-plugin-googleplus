9edae102d6882e4e12ceec31090d1c6330ce451a
Mike Nelson <mike@beweb.co.nz>
2019-09-11 20:22:21 +1200

    oops fixed require syntax from last commit

--- hooks/ios/prerequisites.js
+++ hooks/ios/prerequisites.js
@@ -1,7 +1,7 @@
 console.error("Google Sign-In prerequisites");
 
 module.exports = function (context) {
-  var child_process = context.require('child_process'),
+  var child_process = require('child_process'),
       deferral = require('q').defer();
 
   var output = child_process.exec('npm install', {cwd: __dirname},
