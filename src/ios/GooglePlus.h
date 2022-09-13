a845f72715c8ef6cf855f40d9e4e761c05929f18
Brent Dearth <brent.dearth@gmail.com>
2022-09-13 15:52:14 -0600

    feat(sdk): support latest SDK versions for Android and iOS

--- src/ios/GooglePlus.h
+++ src/ios/GooglePlus.h
@@ -1,16 +1,13 @@
 #import <Cordova/CDVPlugin.h>
 #import <GoogleSignIn/GoogleSignIn.h>
 
-@interface GooglePlus : CDVPlugin<GIDSignInDelegate, GIDSignInDelegate>
+@interface GooglePlus : CDVPlugin
 
 @property (nonatomic, copy) NSString* callbackId;
 @property (nonatomic, assign) BOOL isSigningIn;
 
-- (void) isAvailable:(CDVInvokedUrlCommand*)command;
 - (void) login:(CDVInvokedUrlCommand*)command;
-- (void) trySilentLogin:(CDVInvokedUrlCommand*)command;
 - (void) logout:(CDVInvokedUrlCommand*)command;
 - (void) disconnect:(CDVInvokedUrlCommand*)command;
-- (void) share_unused:(CDVInvokedUrlCommand*)command;
 
 @end
